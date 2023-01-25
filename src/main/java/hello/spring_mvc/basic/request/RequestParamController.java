02173e593149d58e004cbb2dc345377b18337a06
kimtaesoo99 <107785279+kimtaesoo99@users.noreply.github.com>
2023-01-25 12:23:03 +0900

    feat: @ModelAttribute 테스트

--- src/main/java/hello/spring_mvc/basic/request/RequestParamController.java
+++ src/main/java/hello/spring_mvc/basic/request/RequestParamController.java
@@ -1,7 +1,9 @@
 package hello.spring_mvc.basic.request;
 
+import hello.spring_mvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -73,6 +75,19 @@ public class RequestParamController {
         return "ok";
     }
 
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(@ModelAttribute HelloData helloData){
+        log.info("HelloData={}",helloData);
+        return "ok";
+    }
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData){
+        log.info("HelloData={}",helloData);
+        return "ok";
+    }
 
 
 }
